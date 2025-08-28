079adf0951f90d34bca88eea9218d6bb6ce31592
LukasThome <thome_lucas@live.com>
2025-08-28 17:35:09 -0300

    teste de push

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'jv3vn6',
+  projectId: 'wqyuzj',
   e2e: {
     baseUrl:'https://restful-booker.herokuapp.com',
     requestTimeout: 6000,
